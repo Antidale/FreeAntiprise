8763f367365ea7bc06e3ea5532a0255f4c975f76
Antidale <antidale@tellah.life>
2025-07-07 19:13:31 -0700

    removing files that shouldn't be included in the dockerization

--- StartSite.ps1
+++
@@ -1,7 +0,0 @@
-if ((Test-Path env:VIRTUAL_ENV) -eq $false) { 
-    if ((Test-Path ./ENV/Scripts) -eq $false) {
-        pwsh ./ENV/bin/Activate.ps1 && python -m FreeEnt ./ff4.rom.smc server --local
-    }
-
-    ./ENV/Scripts/Activate.ps1 
-}
